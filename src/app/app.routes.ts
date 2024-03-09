38e5b836d7818b4e0c56874b2ac209055186e95d
abhinavgit2002 <abhinavjnair61@gmail.com>
2024-03-09 23:24:39 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  //login
+//   {
+//     path:'',component:LoginComponent
+//   },
+//   //dashboard
+//   {
+//     path:'dashboard',component:DashboardComponent
+//   },
+//   //register
+//   {
+//      path:'register',component:RegisterComponent
+//   },
+//   //transaction
+//   {
+//     path:'transaction',component:TransactionComponent
+//  }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

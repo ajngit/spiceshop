e9d12a7cdfb61744e3096a1a204b7c7d4751f75a
abhinavgit2002 <abhinavjnair61@gmail.com>
2024-03-10 00:32:05 +0530

    spiceshop first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
-  //login
-//   {
-//     path:'',component:LoginComponent
-//   },
+  //home
+  {
+    path:'',component:HomeComponent
+  }
 //   //dashboard
 //   {
 //     path:'dashboard',component:DashboardComponent

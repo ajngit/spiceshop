f5f48bc6976ccb178d7c0c793118b5ef31396265
abhinavgit2002 <abhinavjnair61@gmail.com>
2024-03-10 15:20:30 +0530

    fixed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { DetailsComponent } from './details/details.component';
 
 
 export const routes: Routes = [
   //home
   {
     path:'',component:HomeComponent
-  }
-//   //dashboard
-//   {
-//     path:'dashboard',component:DashboardComponent
-//   },
-//   //register
-//   {
-//      path:'register',component:RegisterComponent
-//   },
-//   //transaction
-//   {
-//     path:'transaction',component:TransactionComponent
-//  }
+  },
+//   //login
+  {
+    path:'login',component:LoginComponent
+  },
+  //register
+  {
+     path:'register',component:RegisterComponent
+  },
+  //categories
+  {
+    path:'categories',component:CategoriesComponent
+ },
+ //details
+ {
+  path:'details',component:DetailsComponent
+}
 
 
 ];

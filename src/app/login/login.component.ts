5d9b85b19bb4e86ef9fc336c69a3b0d4b7850510
abhinavgit2002 <abhinavjnair61@gmail.com>
2024-03-19 22:13:38 +0530

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  pswd: number = 123;
+  email: string = 'abc@gmail.com';
+  constructor() {
+
+  }
+  async ngOnInit() {
+  }
+  login(email:string,pswd:number) {
+    debugger;
+    if (this.pswd === pswd && this.email === email) {
+      alert('Login Success');
+    }
+  }
 
 }

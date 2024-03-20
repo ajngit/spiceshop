4397778794cec169f0e38f3a81d4aa5c18974bfa
ajngit <abhinavcrypto123@gmail.com>
2024-03-20 23:00:44 +0530

    header change and login unit test 3

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -30,4 +30,42 @@ describe('LoginComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h3')?.textContent).toContain('LOGIN');
   });
+
+  it('form invalid when empty', () => {
+    expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('email field validity', () => {
+    let errors: any = {}; 
+    const email = component.loginForm.controls['email'];
+    // expect(email.valid).toBeFalsy();
+
+    // errors = email.errors || {};
+    // expect(errors['required']).toBeTruthy();
+
+    // email.setValue('abc');
+    // errors = email.errors || {};
+    // expect(errors['required']).toBeFalsy();
+    // expect(errors['email']).toBeTruthy();
+
+    email.setValue('abc@gmail.com');
+    errors = email.errors || {};
+    expect(errors['required']).toBeFalsy();
+    expect(errors['email']).toBeFalsy();
+});
+
+it('password field validity', () => {
+    let errors: any = {}; 
+    const password = component.loginForm.controls['password'];
+    expect(password.valid).toBeFalsy();
+
+    errors = password.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    password.setValue('123');
+    errors = password.errors || {};
+    expect(errors['required']).toBeFalsy();
+});
+
+
 });

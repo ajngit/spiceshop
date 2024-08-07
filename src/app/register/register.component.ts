41e3c72262d8354852f1f086ff562091f5efac89
ajngit <abhinavcrypto123@gmail.com>
2024-08-07 20:17:23 +0530

    registration form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Authenticate } from './models/Authenticate';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  UserData : Authenticate = new Authenticate ();
+
+  constructor(){}
+
+  ngOnInit(){
+
+  }
+
+  Submit(){
+    console.log(this.UserData);
+    
+  }
 }
